582dc22fb9fc0571b6d53aeef92b677b22dc6c2a
VIVEK KUMAR <yvksharma@gmail.com>
2024-02-13 16:41:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import {useState} from "react"
+import Modal from "./component/Modal";
+
 
 function App() {
+  const [show,setShow] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className=" relative px-3 py-3">
+      <button className="px-3 border-2 border-red-600 bg-slate-600 transition-[1s]" onClick={()=>setShow(!show)}>Login</button>
+      <div className="fixed left-1/2 top-1/2 " >
+       {show? <Modal/> : null}
+      </div> 
     </div>
+    <table style={{border:"1px solid black",borderCollapse:"collapse"}}>
+      <tr>
+        <th>X</th>
+        <th>Y</th>
+        <th>Z</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>1</td>
+        <td>1</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>1</td>
+        <td>1</td>
+      </tr>
+    </table>
+
+
+      </>  
   );
 }
 
---
+++ src/component/Modal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Modal = () => {
+  return (
+    <div className='w-1/3 h-1/3 bg-slate-500'>
+      yesss
+    </div>
+  )
+}
+
+export default Modal
